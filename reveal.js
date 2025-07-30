c74f5fbedd9ab9e152103ed4a815d0301fff0680
HTAndrade <hugotsandrade@gmail.com>
2025-07-30 13:48:13 +0100

    Add ScrollReveal

---
+++ reveal.js
@@ -0,0 +1,94 @@
+window.revelar = ScrollReveal({reset: true})
+
+revelar.reveal('.home-content h1', {
+  duration: 2000,
+  distance: '50px',
+} )
+
+revelar.reveal(".home-content p", {
+  duration: 2000,
+  distance: "60px",
+  delay: 500,
+})
+
+revelar.reveal(".home-a", {
+  duration: 2000,
+  distance: "60px",
+  delay: 1000,
+})
+
+revelar.reveal(".title", {
+  duration: 2000,
+  distance: "50px",
+})
+
+revelar.reveal(".sobre-content p", {
+  duration: 2000,
+  distance: "50px",
+  delay: 500,
+})
+
+revelar.reveal(".sobre-content img", {
+  duration: 2000,
+  distance: "50px",
+  delay: 1000,
+})
+
+revelar.reveal(".title2", {
+  duration: 2000,
+  distance: "50px",
+})
+
+revelar.reveal(".servico-card1", {
+  duration: 2000,
+  distance: "50px",
+  delay: 200,
+})
+
+revelar.reveal(".servico-card2", {
+  duration: 2000,
+  distance: "50px",
+  delay: 400,
+})
+
+revelar.reveal(".servico-card3", {
+  duration: 2000,
+  distance: "50px",
+  delay: 600,
+})
+
+revelar.reveal(".servico-card4", {
+  duration: 2000,
+  distance: "50px",
+  delay: 800,
+})
+
+revelar.reveal(".servico-card5", {
+  duration: 2000,
+  distance: "50px",
+  delay: 1000,
+})
+
+revelar.reveal(".servico-card6", {
+  duration: 2000,
+  distance: "50px",
+  delay: 1200,
+})
+
+revelar.reveal(".servico-card7", {
+  duration: 2000,   
+  distance: "50px",
+  delay: 1400,
+})
+
+revelar.reveal(".servico-card8", {
+  duration: 2000,
+  distance: "50px",
+  delay: 1600,
+})
+
+revelar.reveal(".servico-card9", {
+  duration: 2000,
+  distance: "50px",
+  delay: 1800,
+})
