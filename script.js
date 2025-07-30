ac632a60114eadfc282e0d426e6afa090522c008
HTAndrade <hugotsandrade@gmail.com>
2025-07-30 02:48:44 +0100

    add script review

---
+++ script.js
@@ -0,0 +1,16 @@
+window.revelar = ScrollReveal({reset: true})
+
+revelar.reveal('.home-content h1', {
+  duration: 2000,
+  distance: '50px',
+} )
+
+revelar.reveal(".home-content p", {
+  duration: 4000,
+  distance: "50px",
+})
+
+revelar.reveal(".home-content a", {
+  duration: 5000,
+  distance: "50px",
+})
