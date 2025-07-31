7ed2464fae0afb40a0347a40715434c657bb3cea
HTAndrade <hugotsandrade@gmail.com>
2025-07-31 23:46:47 +0100

    Update ScrollReveal

--- reveal.js
+++ reveal.js
@@ -1,160 +1,187 @@
 window.revelar = ScrollReveal({reset: true})
 
-revelar.reveal('.home-content h1', {
+revelar.reveal(".home-content h1", {
   duration: 2000,
-  distance: '50px',
-} )
+  distance: "50px",
+  reset: false,
+})
 
 revelar.reveal(".home-content p", {
   duration: 2000,
   distance: "60px",
   delay: 500,
+  reset: false,
 })
 
 revelar.reveal(".home-a", {
   duration: 2000,
   distance: "60px",
   delay: 1000,
+  reset: false,
 })
 
 revelar.reveal(".title", {
   duration: 2000,
   distance: "50px",
+  reset: false,
 })
 
 revelar.reveal(".sobre-content p", {
   duration: 2000,
   distance: "50px",
   delay: 500,
+  reset: false,
 })
 
 revelar.reveal(".sobre-content img", {
   duration: 2000,
   distance: "50px",
   delay: 1000,
+  reset: false,
 })
 
 revelar.reveal(".title2", {
   duration: 2000,
   distance: "50px",
+  reset: false,
 })
 
 revelar.reveal(".title3", {
   duration: 2000,
   distance: "50px",
+  reset: false,
 })
 
 revelar.reveal(".servico-card1", {
   duration: 2000,
   distance: "50px",
   delay: 200,
+  reset: false,
 })
 
 revelar.reveal(".servico-card2", {
   duration: 2000,
   distance: "50px",
   delay: 400,
+  reset: false,
 })
 
 revelar.reveal(".servico-card3", {
   duration: 2000,
   distance: "50px",
   delay: 600,
+  reset: false,
 })
 
 revelar.reveal(".servico-card4", {
   duration: 2000,
   distance: "50px",
   delay: 800,
+  reset: false,
 })
 
 revelar.reveal(".servico-card5", {
   duration: 2000,
   distance: "50px",
   delay: 1000,
+  reset: false,
 })
 
 revelar.reveal(".servico-card6", {
   duration: 2000,
   distance: "50px",
   delay: 1200,
+  reset: false,
 })
 
 revelar.reveal(".servico-card7", {
-  duration: 2000,   
+  duration: 2000,
   distance: "50px",
   delay: 1400,
+  reset: false,
 })
 
 revelar.reveal(".servico-card8", {
   duration: 2000,
   distance: "50px",
   delay: 1600,
+  reset: false,
 })
 
 revelar.reveal(".servico-card9", {
   duration: 2000,
   distance: "50px",
   delay: 1800,
+  reset: false,
 })
 
 revelar.reveal(".contato-texto1", {
   duration: 2000,
   distance: "50px",
   delay: 500,
+  reset: false,
 })
 
 revelar.reveal(".contato-texto2", {
   duration: 2000,
   distance: "50px",
   delay: 1000,
+  reset: false,
 })
 
 revelar.reveal(".contato-texto3", {
   duration: 2000,
   distance: "50px",
   delay: 1500,
+  reset: false,
 })
 
 revelar.reveal(".contato-form", {
   duration: 2000,
   distance: "50px",
   delay: 2000,
+  reset: false,
 })
 
 revelar.reveal(".form-nome", {
   duration: 2000,
   distance: "50px",
   delay: 3000,
+  reset: false,
 })
 
 revelar.reveal(".form-email", {
-  duration: 2000, 
+  duration: 2000,
   distance: "50px",
   delay: 3100,
+  reset: false,
 })
 
 revelar.reveal(".form-msg", {
   duration: 2000,
   distance: "50px",
   delay: 3200,
+  reset: false,
 })
 
 revelar.reveal(".form-telefone", {
-  duration: 2000, 
+  duration: 2000,
   distance: "50px",
   delay: 3300,
+  reset: false,
 })
 
 revelar.reveal(".form-btn", {
   duration: 2000,
   distance: "50px",
   delay: 3400,
+  reset: false,
 })
 
 revelar.reveal("button", {
   duration: 2000,
   delay: 5000,
+  reset: false,
 })
 
 document.querySelector("#year").textContent = new Date().getFullYear();
